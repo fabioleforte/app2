4649aedd19b615d34925b0afa1381041680cc299
Fabio Matias Leforte <fabioleforte@brq.com>
2019-07-25 18:43:17 -0300

    criado componente de ofertas e configurado a rota

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { RestaurantesComponent } from './restaurantes/restaurantes.component';
 import { DiversaoComponent } from './diversao/diversao.component';
 import { RouterModule } from '@angular/router';
 import { ROUTES } from './app.routes';
+import { OfertaComponent } from './oferta/oferta.component';
 
 @NgModule({
   declarations: [
@@ -19,6 +20,7 @@ import { ROUTES } from './app.routes';
     RodapeComponent,
     RestaurantesComponent,
     DiversaoComponent,
+    OfertaComponent,
   ],
   imports: [
 
@@ -27,6 +29,7 @@ import { ROUTES } from './app.routes';
     RouterModule.forRoot(ROUTES)
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [OfertaComponent]
 })
 export class AppModule { }
