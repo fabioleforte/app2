ff08bdf1edf0cab66bd724fd75936a3ee68bc970
Fabio Matias Leforte <fabioleforte@brq.com>
2019-07-25 19:00:01 -0300

    criado link para ofertas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,6 +9,7 @@ export const ROUTES: Routes = [
   { path: 'restaurantes', component: RestaurantesComponent },
   { path: 'diversao', component: DiversaoComponent },
   { path: 'oferta', component: OfertaComponent },
+  { path: 'oferta/:id', component: OfertaComponent },
 ];
 
 
