a32c90be30ab3026214e0d26b7ee410a8a5f1b56
Fabio Matias Leforte <fabioleforte@brq.com>
2019-07-26 13:59:12 -0300

    criado const url_api

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Oferta } from './shared/oferta.model';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/toPromise';
+import { URL_API } from './app.api';
 
 @Injectable()
 export class OfertasService {
@@ -9,23 +10,22 @@ export class OfertasService {
   constructor(private http: Http) { }
 
   public getOfertas(): Promise<Oferta[]> {
-    return this.http.get('http://localhost:3000/ofertas?destaque=true')
+    return this.http.get(`${URL_API}?destaque=true`)
       .toPromise()
       .then((resposta: any) => resposta.json());
   }
 
   public getOfertasPorCategoria(categoria: string): Promise<Oferta[]> {
-    return this.http.get(`http://localhost:3000/ofertas?categoria=${categoria}`)
+    return this.http.get(`${URL_API}?categoria=${categoria}`)
       .toPromise()
       .then((resposta: any) => resposta.json());
   }
 
   public getOfertaPorId(id: number): Promise<Oferta> {
-    return this.http.get(`http://localhost:3000/ofertas?id=${id}`)
+    return this.http.get(`${URL_API}?id=${id}`)
       .toPromise()
       .then((resposta: any) => {
         return resposta.json().shift();
       });
   }
-
 }
