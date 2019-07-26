3a67750331aaa2255dc4ce7db5f80bf1aa316810
Fabio Matias Leforte <fabioleforte@brq.com>
2019-07-26 11:12:05 -0300

    [oferta] criado método de oferta por id

--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { OfertasService } from '../ofertas.service';
+import { Oferta } from '../shared/oferta.model';
 
 @Component({
   selector: 'app-oferta',
   templateUrl: './oferta.component.html',
-  styleUrls: ['./oferta.component.css']
+  styleUrls: ['./oferta.component.css'],
+  providers: [OfertasService]
 })
 export class OfertaComponent implements OnInit {
 
-  constructor() { }
+  public oferta: Oferta;
+
+
+  constructor(private route: ActivatedRoute, private ofertaservice: OfertasService) { }
 
   ngOnInit() {
+
+    this.ofertaservice.getOfertaPorId(this.route.snapshot.params['id'])
+      .then((ofertas: Oferta) => {
+        this.oferta = ofertas;
+        // console.log(this.oferta);
+
+      });
   }
 
 }
