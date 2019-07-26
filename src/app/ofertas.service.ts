122f465764d3c633e81be81596b184f9fd081df1
Fabio Matias Leforte <fabioleforte@brq.com>
2019-07-26 18:23:20 -0300

    [ofertas] criado método de pesquisa

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@angular/core';
 import { Oferta } from './shared/oferta.model';
-import { Http } from '@angular/http';
-import 'rxjs/add/operator/toPromise';
+import { Http, Response } from '@angular/http';
 import { URL_API } from './app.api';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/retry';
 
 @Injectable()
 export class OfertasService {
@@ -12,19 +15,19 @@ export class OfertasService {
   public getOfertas(): Promise<Oferta[]> {
     return this.http.get(`${URL_API}/ofertas?destaque=true`)
       .toPromise()
-      .then((resposta: any) => resposta.json());
+      .then((resposta: Response) => resposta.json());
   }
 
   public getOfertasPorCategoria(categoria: string): Promise<Oferta[]> {
     return this.http.get(`${URL_API}/ofertas?categoria=${categoria}`)
       .toPromise()
-      .then((resposta: any) => resposta.json());
+      .then((resposta: Response) => resposta.json());
   }
 
   public getOfertaPorId(id: number): Promise<Oferta> {
     return this.http.get(`${URL_API}/ofertas?id=${id}`)
       .toPromise()
-      .then((resposta: any) => {
+      .then((resposta: Response) => {
         return resposta.json().shift();
       });
   }
@@ -32,7 +35,7 @@ export class OfertasService {
   public getComoUsarOfertaPorId(id: number): Promise<string> {
     return this.http.get(`${URL_API}/como-usar? id = ${id}`)
       .toPromise()
-      .then((resposta: any) => {
+      .then((resposta: Response) => {
         return resposta.json()[0].descricao;
       });
   }
@@ -40,8 +43,16 @@ export class OfertasService {
   public getOndeFicaPorId(id: number): Promise<string> {
     return this.http.get(`${URL_API}/onde-fica? id = ${id}`)
       .toPromise()
-      .then((resposta: any) => {
+      .then((resposta: Response) => {
         return resposta.json()[0].descricao;
+      });
+  }
+
+  public pesquisaOfertas(termo: string): Observable<Oferta[]> {
+    return this.http.get(`${URL_API}/ofertas?descricao_oferta_like=${termo}`)
+      .retry(10)
+      .map((resposta: Response) => {
+        return resposta.json();
       });
   }
 }
--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { OfertasService } from '../ofertas.service';
+import { Oferta } from '../shared/oferta.model';
+import { Subject } from 'rxjs/Subject';
+import '../util/rxjs-extensions';
 
 @Component({
   selector: 'app-topo',
   templateUrl: './topo.component.html',
-  styleUrls: ['./topo.component.css']
+  styleUrls: ['./topo.component.css'],
+  providers: [OfertasService]
 })
 export class TopoComponent implements OnInit {
 
-  constructor() { }
+  public ofertas: Observable<Oferta[]>;
+  public ofertas2: Oferta[];
+  private subjectPesquisa: Subject<string> = new Subject();
+
+  constructor(private ofertaService: OfertasService) { }
 
   ngOnInit() {
+    this.ofertas = this.subjectPesquisa
+      .debounceTime(1000)
+      .distinctUntilChanged()
+      .switchMap((termo: string) => {
+        console.log('Requisação http para api: ', termo);
+        if (termo.trim() === '') {
+          return Observable.of<Oferta[]>([]);
+        }
+        return this.ofertaService.pesquisaOfertas(termo);
+      })
+      .catch((err: any) => {
+        console.log(err);
+        return Observable.of<Oferta[]>([]);
+      })
+
+    this.ofertas.subscribe((ofertas: Oferta[]) => {
+
+      this.ofertas2 = ofertas;
+    });
+  }
+
+  public pesquisa(termoDaBusca: string): void {
+    console.log('Keyup caracter: ', termoDaBusca);
+    this.subjectPesquisa.next(termoDaBusca);
   }
 
 }
